5e51e6bbe68b88119d6696895b49e7928b5fb047
abdul-aziz <abdulaziz.eu1@gmail.com>
2023-09-28 01:24:15 +0600

    update product operation successfully

--- BookStoreWeb/Areas/Admin/Controllers/ProductController.cs
+++ BookStoreWeb/Areas/Admin/Controllers/ProductController.cs
@@ -21,10 +21,7 @@ namespace BookStoreWeb.Areas.Admin.Controllers
         }
 
         public IActionResult Index()
-        {
-            
-
-
+        {            
             return View();
         }
 
@@ -49,17 +46,16 @@ namespace BookStoreWeb.Areas.Admin.Controllers
 
 
             if(id== 0 || id == null){
-
-                //Create Product
               
                 return View(productVM);
             }
 
             else
             {
-                //Update
+                productVM.Product = _unitOfWork.Product.GetFirstOrDefault(u =>u.Id==id);
+                return View(productVM);
             }
-            return View(productVM);
+            
         }
 
         [HttpPost]
@@ -75,6 +71,15 @@ namespace BookStoreWeb.Areas.Admin.Controllers
                     var uploads = Path.Combine(wwwRootPath, @"images\products");
                     var extension = Path.GetExtension(file.FileName);
 
+                    if (obj.Product.ImageUrl != null)
+                    {
+                        var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
+                       if(System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
                     using (var fileStrem =
                         new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
                     {
@@ -82,7 +87,17 @@ namespace BookStoreWeb.Areas.Admin.Controllers
                     }
                     obj.Product.ImageUrl = @"images\products" + fileName + extension;
                 }
-                _unitOfWork.Product.Add(obj.Product);
+
+                if(obj.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(obj.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(obj.Product);
+                }
+
+               
                 _unitOfWork.Save();
                 return RedirectToAction("Index");
 
--- BookStoreWeb/wwwroot/js/product.js
+++ BookStoreWeb/wwwroot/js/product.js
@@ -17,7 +17,19 @@ function loadDataTable() {
             { "data": "author", "width": "15%" },
             { "data": "catagory.name", "width": "15%" },
             { "data": "price", "width": "15%" },
-            
+            {
+                "data": "id",
+                "render": function (data) {
+
+                    return `
+                     <a href="/Admin/Product/Upsert?id=${data}"  class="btn btn-primary">Edit</a>
+                        <a  class="btn btn-danger">Delete</a>
+                    `
+                },
+
+
+                "width": "15%"
+            },
         ]
 
     });
