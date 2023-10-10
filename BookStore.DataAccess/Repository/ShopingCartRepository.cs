72f262a096d2c50223f5ee3b8e6e4a26c8717d0b
abdul-aziz <abdulaziz.eu1@gmail.com>
2023-10-11 00:52:23 +0600

    add order header model

---
+++ BookStore.DataAccess/Repository/ShopingCartRepository.cs
@@ -0,0 +1,43 @@
+﻿using BookStore.Data;
+using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using BookStore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccess.Repository
+{
+    public class ShopingCartRepository : Repository<ShopingCart>, IShopingCartRepository
+    {
+        public ApplicationDbContext _db;
+
+        public ShopingCartRepository(ApplicationDbContext db) :base(db)
+        {
+            _db = db;
+        }
+
+		public int DecrementCount(ShopingCart shopingCart, int count)
+		{
+            shopingCart.Count -= count;
+            return shopingCart.Count;
+		}
+
+		public int IncrementCount(ShopingCart shopingCart, int count)
+		{
+			shopingCart.Count += count;
+			return shopingCart.Count;
+		}
+
+		public int UpdateCount(ShopingCart shopingCart, int count)
+        {
+                shopingCart.Count = count;
+                return shopingCart.Count;
+            }
+        }
+
+        
+    }
+
---
+++ BookStore.Models/OrderHeader.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+	public class OrderHeader
+	{
+		public int Id { get; set; }
+
+		public string ApplicationUserId { get; set; }
+
+		[ForeignKey(nameof(ApplicationUserId))]
+		[ValidateNever]
+		public ApplicationUser ApplicationUser { get; set; }
+
+		[Required]
+		public DateTime OrderDate{ get; set; }
+		public DateTime ShippingDate { get; set; }
+
+		public double OrderTotal { get; set; }
+
+		public string? OrderStatus { get; set; }
+		public string? PaymentStatus { get; set; }
+
+        public string?  TrackingNumber { get; set; }
+		public string? Carrer { get; set; }
+
+		public DateTime PaymentDate { get; set; }
+		public DateTime PaymentDueDate { get; set; }
+
+		public string SessionId { get; set; }
+		public string PaymentIntentNumber { get; set; }
+
+		[Required]
+		public String Name { get; set; }
+		[Required]
+		public String? City { get; set; }
+		[Required]
+		public String? Street { get; set; }
+		[Required]
+
+		public String? StreetAddress { get; set; }
+		[Required]
+		public String? PostalCode { get; set; }
+	}
+
+}
